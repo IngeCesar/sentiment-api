f880fba51e37c46259e3a08ebec3f75529c44ead
IngeCesar <cooh1@ymail.com>
2025-12-27 15:15:08 -0600

    sustitui el .zip por la carpeta descomprimida del proyecto y agregue la configuracion del proyecto en un .pdf

---
+++ Backend/sentimientapi/sentimientapi/src/test/java/com/tusentimiento/sentimientapi/SentimientapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tusentimiento.sentimientapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SentimientapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
